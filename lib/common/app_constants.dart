db8ec5b9640b0febb4ccb187e987cf7e59cb3edd
mg3994 <manishgautammg7@yahoo.com>
2025-02-07 15:13:13 +0530

    fixed app constants and jks

--- lib/common/app_constants.dart
+++ lib/common/app_constants.dart
@@ -5,19 +5,18 @@ import 'package:restart_tagxi/db/app_database.dart';
 import '../features/language/domain/models/language_listing_model.dart';
 
 class AppConstants {
-  static const String title = 'product name';
-  static const String baseUrl = 'your base url here';
+  static const String title = 'DroppingRide';
+  static const String baseUrl = 'https://onboarding.droppingride.com';
   static String firbaseApiKey = (Platform.isAndroid)
-      ? "android firebase api key"
+      ? "AIzaSyCMyZR-VFeMh4uc9aGMn1Bi0pjcw2jmbgo"
       : "ios firebase api key";
-  static String firebaseAppId =
-      (Platform.isAndroid) ? "android firebase app id" : "ios firebase app id";
-  static String firebasemessagingSenderId = (Platform.isAndroid)
-      ? "android firebase sender id"
-      : "ios firebase sender id";
-  static String firebaseProjectId = (Platform.isAndroid)
-      ? "android firebase project id"
-      : "ios firebase project id";
+  static String firebaseAppId = (Platform.isAndroid)
+      ? "1:331750600814:android:86373cf08e69c20aaef67f"
+      : "ios firebase app id";
+  static String firebasemessagingSenderId =
+      (Platform.isAndroid) ? "331750600814" : "ios firebase sender id";
+  static String firebaseProjectId =
+      (Platform.isAndroid) ? "dropping-app-2025" : "ios firebase project id";
 
   static String mapKey =
       (Platform.isAndroid) ? "android map key" : 'ios map key';
@@ -30,8 +29,9 @@ class AppConstants {
     LocaleLanguageList(name: 'French', lang: 'fr'),
     LocaleLanguageList(name: 'Spanish', lang: 'es')
   ];
-  static String packageName = '';
-  static String signKey = '';
+  static String packageName = 'com.droppingride.driver';
+  static String signKey =
+      '91:DE:F4:F1:B0:57:E0:D3:1F:6E:B3:09:A1:FA:65:86:82:A7:DC:93';
   double headerSize = 18.0;
   double subHeaderSize = 16.0;
   double buttonTextSize = 20.0;
